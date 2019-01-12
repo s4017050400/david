75e1b7ae470d8d4b68ad75e082b1826f4f7e2831
s4017050400 <s4017050400@gmail.com>
2019-01-12 19:46:50 +0800

    Update time_clock.py

--- time_clock.py
+++ time_clock.py
@@ -5,8 +5,8 @@ today=date.today() #which is like datetime.date(2018,10,3)
 one_day=timedelta(days=1) #one day which can be plused
 
 for i in range(1,11):
-  date=today+i*one_day
-  standard=date.isoformat().translate({ord(c):None for c in '-'})#date.isoformat() is 2018-10-03
+  date1=today+i*one_day
+  standard=date1.isoformat().translate({ord(c):None for c in '-'})#date.isoformat() is 2018-10-03
   print(standard)
   
 '''
