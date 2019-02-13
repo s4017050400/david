213c38272d93d8862410dc37905721ab22b8aa2e
s4017050400 <s4017050400@gmail.com>
2019-02-13 22:30:24 +0800

    Update tkinter.py

--- tkinter.py
+++ tkinter.py
@@ -7,3 +7,23 @@ root.withdraw()
 file_path = filedialog.askopenfilename()
  
 print(file_path)　# 打印文件的路径
+
+
+
+#__________________________________________
+#方法寫法
+import tkinter as tk
+from tkinter import filedialog
+ 
+root = tk.Tk()
+root.withdraw()
+ 
+file_path = filedialog.askopenfilename()
+
+class Path():
+    def __init__(self, path2):
+        self.path2 = path2
+        
+b=Path(file_path)
+
+print(b.path2)
