11d4f49c05f01ae6aee12f89711b0c09ab20468e
s4017050400 <s4017050400@gmail.com>
2019-02-15 14:10:00 +0800

    Update creating_path.py

--- creating_path.py
+++ creating_path.py
@@ -1,6 +1,6 @@
 import os
 
 filename = "/foo/bar/baz.txt"¨ #first '/' means a 'C:' if no 'no' means 'wtcwd'
-os.makedirs(os.path.dirname(filename), exist_ok=True) 'exist_ok means judged file-exiting 
+os.makedirs(os.path.dirname(filename), exist_ok=True) 'exist_ok means judged file-existing 
 with open(filename, "w") as f:
     f.write("FOOBAR")
