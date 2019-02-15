9956e7d9495739481b6ff751857b4b66ebefbf0d
s4017050400 <s4017050400@gmail.com>
2019-02-15 16:57:18 +0800

    Update creating_path.py

--- creating_path.py
+++ creating_path.py
@@ -32,16 +32,46 @@ mypath = r'C:\Users\CKLu0\Desktop\UAA077 DD1C corner EV\eda_file_search_2019-02-
 
 
 #______________________________________________________
+#pamo.py
 
+import tkinter as tk
+from tkinter import filedialog
+
+root = tk.Tk()
+root.withdraw()
+
+
+
+
+def path():
+    file = filedialog.askopenfilename()
+    return str(file)
+
+def direct():
+    direct = filedialog.askdirectory()
+    return str(direct)
+
+#______________________________________________________
+import pamo #自己弄的檔案↑
 from os import listdir
->>> mypath = r'C:\Users\CKLu0\Desktop\壓縮檔' #輸入檔案位置
->>> files = listdir(mypath) #把目錄下的檔案列出
->>> for file in files: #列出
-...     d1=dict1[file[17:20]]   #dict1={'FT1':'FT1_-40','FT2':'FT2_25','FT3':'FT3_90','FT':'FT4_125'}
-...     d2=dict2[file[11:13]]   #dict2={'ZW':'HH','ZU':'LL','ZR':'TT','ZT':'HL','ZS':'LH'}
-...     filename = "foo/bar/{0}/{1}/This_is_{0}+{1}_dir.txt".format(d1,d2)
-...     os.makedirs(os.path.dirname(filename), exist_ok=True)   #創造路徑
-...     with open(filename, "w") as f1:
-...             f=open(mypath+'/'+file)
-...             f1.write(f.read())
-...             f1.close
+import os
+
+
+dict1={'FT1':'FT1_-40','FT2':'FT2_25','FT3':'FT3_90','FT4':'FT4_125'}
+dict2={'ZW':'HH','ZU':'LL','ZR':'TT','ZT':'HL','ZS':'LH'}
+
+mypath = pamo.direct() #輸入資料目录路径
+outpath = pamo.direct() #輸出資料目录路径
+files = listdir(mypath) #把目錄下的檔案列出
+
+for file in files: #列出
+    d1=dict1[file[17:20]]   #dict1={'FT1':'FT1_-40','FT2':'FT2_25','FT3':'FT3_90','FT4':'FT4_125'}
+    d2=dict2[file[11:13]]   #dict2={'ZW':'HH','ZU':'LL','ZR':'TT','ZT':'HL','ZS':'LH'}
+    
+    filename = outpath+"/{0}/{1}/This_is_{0}+{1}_dir.txt".format(d1,d2)
+    os.makedirs(os.path.dirname(filename), exist_ok=True)   #創造路徑
+    with open(filename, "w") as f1:
+        f=open(mypath+'/'+file)
+        f1.write(f.read())
+        f1.close
+
